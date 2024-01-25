6dd060cdeda55ae6bfa456975d248ff620fae728
Laurens Bultynck <laurens.bultynck@gmail.com>
2024-01-25 10:03:33 +0100

    Use logger trait to clean up class

--- src/Logging/HttpLogger.php
+++ src/Logging/HttpLogger.php
@@ -7,10 +7,13 @@ use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
 use Stringable;
 
 class HttpLogger implements LoggerInterface
 {
+    use LoggerTrait;
+
     private string $token;
 
     private ClientInterface $http;
@@ -31,51 +34,11 @@ class HttpLogger implements LoggerInterface
         $this->streamFactory = $streamFactory;
     }
 
-    public function emergency(string|Stringable $message, array $context = []): void
-    {
-        $this->log('emergency', $message, $context);
-    }
-
-    public function alert(string|Stringable $message, array $context = []): void
-    {
-        $this->log('alert', $message, $context);
-    }
-
-    public function critical(string|Stringable $message, array $context = []): void
-    {
-        $this->log('critical', $message, $context);
-    }
-
-    public function error(string|Stringable $message, array $context = []): void
-    {
-        $this->log('error', $message, $context);
-    }
-
-    public function warning(string|Stringable $message, array $context = []): void
-    {
-        $this->log('warning', $message, $context);
-    }
-
-    public function notice(string|Stringable $message, array $context = []): void
-    {
-        $this->log('notice', $message, $context);
-    }
-
-    public function info(string|Stringable $message, array $context = []): void
-    {
-        $this->log('info', $message, $context);
-    }
-
-    public function debug(string|Stringable $message, array $context = []): void
-    {
-        $this->log('debug', $message, $context);
-    }
-
     public function log($level, string|Stringable $message, array $context = []): void
     {
         $request = $this->requestFactory
             ->createRequest('POST', 'https://apiboard.dev/api/logs')
-            ->withHeader('Authorization', 'Bearer '.$this->token)
+            ->withHeader('Authorization', 'Bearer ' . $this->token)
             ->withHeader('Accept', 'application/json')
             ->withHeader('Content-Type', 'application/json')
             ->withBody($this->streamFactory->createStream(json_encode([
